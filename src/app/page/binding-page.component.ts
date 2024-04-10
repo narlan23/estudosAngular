951f911df9d92ea55fb43962a3aac3dd485ec7d7
Narlan Mota <narlanmota19@gmail.com>
2024-04-10 12:11:51 -0300

    Class binding para aplicar estilos

--- src/app/page/binding-page.component.ts
+++ src/app/page/binding-page.component.ts
@@ -5,10 +5,24 @@ import { Component } from "@angular/core";
   template: `
     <h1>Imagem do Angular</h1>
     <img [src]="imagem" [style]="{width:width}" />
-  `
+
+    <div class="alert" [class]="{success:sucesso}">Alerta</div>
+
+    <button (click)="enviarDados()">Enviar Dados</button>
+  `,
+  styles: [
+    '.alert { width: 200px; height: 100px; border: 1px solid blue; }',
+    '.success { background-color: green; }'
+  ]
+
 })
 export class BindingPageComponent{
   width = "300px"
   imagem = "https://angular.io/assets/images/logos/angular/angular.svg"
+  sucesso = true
+
+  enviarDados(){
+    this.sucesso = !this.sucesso
+  }
 
 }
